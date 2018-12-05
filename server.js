5ca76226868b76bafb59defccd34f32bf7826533
ducanhle <anhld1@gemvietnam.com>
2018-12-05 16:51:07 +0700

    Fix 504 error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const prerender = require('prerender');
 const server = prerender({
     waitAfterLastRequest: 1000,
-    chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
+    chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage' ],
     // chromeLocation: '/usr/bin/google-chrome-stable'
 });
 
